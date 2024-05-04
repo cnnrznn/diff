c1f9ebfcebae9c215e091d70072195bc8dc5a219
Connor Zanin <cnnrznn@gmail.com>
2024-05-03 21:15:36 -0600

    New test for a case that explod(ed) the runtime

--- diff_test.go
+++ diff_test.go
@@ -27,3 +27,23 @@ func TestMiddleEdit(t *testing.T) {
 	seq := Diff(s1, s2)
 	fmt.Println(seq.Steps)
 }
+
+func TestDocs(t *testing.T) {
+	s1 := `This is a document discussing many important topics.
+
+	Such as how to bake cakes; it's easy!
+	
+	1. step one
+	2. step two
+	3. step three	
+	`
+	s2 := `This is a document discussing many important topics.
+
+	Such as how to make cakes; it's easy!
+	2. step two
+	3. step three	
+	`
+
+	seq := Diff(s1, s2)
+	fmt.Println(seq.Steps)
+}
